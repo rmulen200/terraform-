4ebbef174ce71ed138db0701e85a10c657ea91eb
Rachel <rachel87mul@gmail.com>
2022-12-28 17:23:17 -0600

    terraform code

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "terraform-demo" {
+    ami = var.ami
+    instance_type = var.instance-type
+    key_name = var.keypair
+    tags = {
+        Name = "Jenkins"
+        Env = "Dev"
+    }
+}
+
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+region = "us-east-1"
+ami    = "ami-0e1d30f2c40c4c701"
+keypair = "awsdemo"
+instance-type = "t2.micro"
